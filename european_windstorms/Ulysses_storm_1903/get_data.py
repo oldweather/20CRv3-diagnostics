05de9e7e928a3e213795a4e2a9e0613ef601da2a
Philip Brohan <philip@brohan.org>
2018-05-18 16:05:24 +0100

    Typo fix in 1903 data retrieval.

--- european_windstorms/Ulysses_storm_1903/get_data.py
+++ european_windstorms/Ulysses_storm_1903/get_data.py
@@ -1,6 +1,6 @@
 import Meteorographica.data.twcr as twcr
 
 for version in ('2c','4.5.1'):
-    twcr.fetch('prmsl',1953,2,version=version)
-    twcr.fetch_observations(1953,2,version=version)
+    twcr.fetch('prmsl',1903,2,version=version)
+    twcr.fetch_observations(1903,2,version=version)
 
