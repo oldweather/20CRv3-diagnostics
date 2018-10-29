389fa14c8a55fef293d22fbf647b1d81cd5ef88e
Philip Brohan <philip@Philips-MacBook-Pro.local>
2018-10-29 11:02:15 +0000

    Updated setup to iris2.2

--- setup.py
+++ setup.py
@@ -47,13 +47,15 @@ setup(
 
     # Other packages that your project depends on.
     install_requires=[
+        'scitools-iris>=2.2',
+        'cartopy>=0.16',
         'IRData>0.0',
         'Meteorographica>0.0',
-        'numpy>1.13',
-        'scipy>0.18',
-        'pandas>0.20',
+        'numpy>=1.15.2',
+        'scipy>=1.1.0',
+        'pandas>==0.23.4',
         'scikit-learn>0.19',
-        'matplotlib>1.5,<2.0',
+        'matplotlib>=2.2.3',
     ],
 
 
