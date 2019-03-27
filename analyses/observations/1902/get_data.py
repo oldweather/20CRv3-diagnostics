eb84d224c69f741abfc457c17c6b589bdc5a7a63
Philip Brohan <philip@brohan.org>
2019-03-27 16:39:37 +0000

    Added 1902 obs only video.

---
+++ analyses/observations/1902/get_data.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+import IRData.twcr as twcr
+import datetime
+
+dte=datetime.datetime(1902,8,1)
+
+twcr.fetch('observations',dte,version='4.5.1')
+twcr.fetch('prmsl',dte,version='4.5.1')
+
