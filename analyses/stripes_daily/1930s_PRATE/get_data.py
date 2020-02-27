a8db1168eb72a5757a71da27baeed89e47b0782b
Philip Brohan <philip@brohan.org>
2020-02-27 16:10:04 +0000

    Started on daily stripes plots.

---
+++ analyses/stripes_daily/1930s_PRATE/get_data.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+# Download the 20CRv3 sub-daily PRATE.
+
+import IRData.twcr as twcr
+import datetime
+
+for year in range(1926,1935):
+    twcr.fetch('PRATE',datetime.datetime(year,1,1),version='3')
+
