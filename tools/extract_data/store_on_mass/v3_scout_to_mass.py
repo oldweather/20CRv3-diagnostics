3f85e7f8487710f0a01de4fb3feec7e8f2ffa6cf
Philip Brohan <philip@brohan.org>
2021-01-11 17:48:21 +0000

    Added script to store scout monthly data on MASS.

--- tools/extract_data/store_on_mass/v3_scout_to_mass.py
+++ tools/extract_data/store_on_mass/v3_scout_to_mass.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 
-# Archive downloaded 20CRv3 data in MASS
+# Archive downloaded 20CRv3 scout data to MASS
 
 import sys
 import os
 import subprocess
 import os.path
 import glob
+import tarfile
 sys.path.append('/data/users/afterburner/software/turbofan/v1.3.1/lib/python/')
 import afterburner.io.moose2 as moose
 
@@ -22,8 +23,7 @@ parser.add_argument("--version", help="Version, e.g. '4.5.1'",
                     default='4.5.1')
 parser.add_argument("--variable", 
                     help="Variable name ('prmsl','observations,...)",
-                    type=str,required=False,
-                    default='all')
+                    type=str,required=True)
 args = parser.parse_args()
 
 # Check moose availability
@@ -45,69 +45,75 @@ if moose.run_moose_command('moo test %s' % moose_dir)[0]!='true':
 # Disc data dir
 ddir="%s/20CR/version_%s" % (os.environ['SCRATCH'],args.version)
 
+# Archive a field
+def archive_fields(var,year,month,version):
+    if moose.run_moose_command("moo test -f %s/%s_%04d%02d.tar" % 
+                               (moose_dir,var,year,month))[0]=='true':
+        return()  # Already archived
+    # Are there any fields to archive?
+    ofiles=glob.glob("%s/%04d/%s.%04d%02d_mem*.nc" %
+                           (ddir,year,var,year,month))
+    if len(ofiles)==0:  # No fields on disc
+        raise Exception("No %s fields on disc for %04d-%02d %s" %
+                                            (var,year,month,version))
+        return()
+
+    # Pack the month's fields into a single file
+    otarf=("%s/%04d/%s_%04d%02d.tar" % 
+                           (ddir,year,var,year,month))
+    tf = tarfile.open(name=otarf,mode='w')
+    blen = len(ddir)
+    for of in ofiles:
+        tf.add(of,arcname=of[blen:])
+    tf.close()
+
+    # Stow the ob file on MASS
+    moose.put("%s/%04d" % (ddir,year),
+              ['%s_%04d%02d.tar' % (var,year,month)],
+              moose_dir)
+
+    # Clean up
+    os.remove(otarf)
+
+
 # Archive the observations
-def archive_obs(year,month,version,variable):
-    if moose.run_moose_command("moo test -f %s/observations.tgz" % 
-                               moose_dir)[0]=='true':
+def archive_obs(year,month,version):
+    if moose.run_moose_command("moo test -f %s/observations_%04d%02d.tgz" % 
+                               (moose_dir,year,month))[0]=='true':
         return()  # Already archived
 
     # Are there any observations to archive?
     ofiles=glob.glob("%s/observations/%04d/%04d%02d*.txt" %
                            (ddir,year,year,month))
     ofiles.extend(glob.glob("%s/observations/%04d/%04d%02d*.txt.gz" %
-                           (ddir,year,year,month))
+                           (ddir,year,year,month)))
     ofiles.extend(glob.glob("%s/observations/%04d/%04d%02d*/*.txt" %
                            (ddir,year,year,month)))
     ofiles.extend(glob.glob("%s/observations/%04d/%04d%02d*/*.txt.gz" %
                            (ddir,year,year,month)))
     if len(ofiles)==0:  # No obs on disc
-        if variable=='observations':
-            raise Exception("No obs on disc for %04d-%02d %s" %
-                                                (year,month,version))
+        raise Exception("No obs on disc for %04d-%02d %s" %
+                                            (year,month,version))
         return()
 
     # Pack the month's obs into a single file
-    otarf=("%s/observations/%04d/observations.tgz" % 
-                           (ddir,year))
-    proc = subprocess.Popen(
-            "cd %s ; tar czf %s observations/%04d/%04d%02d*.txt" 
-               % (ddir,otarf,year,year,month),
-               stdout=subprocess.PIPE,
-               stderr=subprocess.PIPE,
-               shell=True)
-    (out, err) = proc.communicate()
-    if len(err)!=0:
-        print(err)
-        raise Exception("Failed to tar observations")
+    otarf=("%s/observations/%04d/observations_%04d%02d.tgz" % 
+                           (ddir,year,year,month))
+    tf = tarfile.open(name=otarf,mode='w:gz')
+    blen = len(ddir)
+    for of in ofiles:
+        tf.add(of,arcname=of[blen:])
+    tf.close()
 
     # Stow the ob file on MASS
-    moose.put("%s/observations/%04d" % (ddir,year),
-              ['observations.tgz'],
+    moose.put("%s/%04d" % (ddir,year),
+              ['observations_%04d%02d.tgz' % (year,month)],
               moose_dir)
 
     # Clean up
     os.remove(otarf)
 
-# Archive a variable file
-def archive_variable(year,month,version,variable):
-    if moose.run_moose_command("moo test %s/%s.nc" % 
-                               (moose_dir,variable))[0]=='true':
-        return()  # Already archived
-    vf="%s/%04d/%02d/%s.nc" % (ddir,year,month,variable)
-    if not os.path.isfile(vf):
-        raise Exception("No data file %s" % vf)
-    moose.put("%s/%04d/%02d" % (ddir,year,month),
-              ["%s.nc" % variable],
-              moose_dir) 
-
-
-if args.variable=='all':
-    vars=glob.glob("%s/%04d/%02d/*.nc" % (ddir,args.year,args.month))
-    for var in vars:
-        variable=os.path.splitext(os.path.basename(var))[0]
-        archive_variable(args.year,args.month,args.version,variable)
-    archive_obs(args.year,args.month,args.version,args.variable)
-elif args.variable=='observations':
-    archive_obs(args.year,args.month,args.version,args.variable)
+if args.variable=='observations':
+    archive_obs(args.year,args.month,args.version)
 else:
-    archive_variable(args.year,args.month,args.version,args.variable)
+    archive_fields(args.variable,args.year,args.month,args.version)
