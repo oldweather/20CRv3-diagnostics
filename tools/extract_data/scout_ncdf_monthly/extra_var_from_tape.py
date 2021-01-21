a6365d32f612d439dcee59bcc6b7edcf9324a12f
Philip Brohan <philip@brohan.org>
2021-01-21 14:28:18 +0000

    Added script to extract extra variables from v3 scout runs.

--- tools/extract_data/scout_ncdf_monthly/extra_var_from_tape.py
+++ tools/extract_data/scout_ncdf_monthly/extra_var_from_tape.py
@@ -30,54 +30,11 @@ working_directory = "%s/20CRv3.working.nc/version_%03d/%04d/%02d" % (
 if not os.path.isdir(working_directory):
     os.makedirs(working_directory)
 
-# Get the obs files from the grib2 archive (on hsi)
-originals_directory = (
-    "/home/projects/incite11/ensda_" + "v%03d_archive_grb2_monthly" % args.version
-)
-# Get the list of start years
-proc = subprocess.Popen(
-    "hsi ls -l %s" % originals_directory,
-    stdout=subprocess.PIPE,
-    stderr=subprocess.PIPE,
-    shell=True,
-)
-(out, err) = proc.communicate()
-start_years = err.decode("utf8").split()  # Why does hsi put output in stderr?
-start_years = [x for x in start_years if "ensda_%03d" % args.version in x]
-start_years = [int(x[-4:]) for x in start_years]
-if args.startyear not in start_years:
-    raise Exception("%04d is not as available start year" % args.startyear)
-
-# Get the list of files available for the selected month
-proc = subprocess.Popen(
-    "hsi ls -l %s/ensda_%03d_%04d/%04d/%04d%02d_*.tar"
-    % (
-        originals_directory,
-        args.version,
-        args.startyear,
-        args.year,
-        args.year,
-        args.month,
-    ),
-    stdout=subprocess.PIPE,
-    stderr=subprocess.PIPE,
-    shell=True,
-)
-(out, err) = proc.communicate()
-file_list = err.decode("utf8").split()
-obs_files = [x for x in file_list if "_psobs" in x]
-if len(obs_files) < 1:
-    raise Exception("Obs files not available in grib2 archive")
 
 # Have the list of files to retrieve - sort them into tape order
 tfile = tempfile.NamedTemporaryFile(delete=False, mode="w")
-for fn in obs_files:
-    tfile.write(
-        "%s/ensda_%03d_%04d/%04d/%s\n"
-        % (originals_directory, args.version, args.startyear, args.year, fn)
-    )
 var_files = []
-for var in ("PRMSL", "PRATE", "TMP2m", "UGRD10m", "VGRD10m", "TMPS", "PWAT", "WEASD"):
+for var in ("PWAT", "WEASD"):
     var_files.append(
         "%s_%04d%02d_v3_x%03d.tar" % (var, args.year, args.month, args.version)
     )
@@ -101,7 +58,7 @@ proc = subprocess.Popen(
 # if out is not None or err is not None:
 # raise StandardError("Problem with hsi tape ordering")
 
-# Run the obs files retrieval
+# Run the  retrieval
 sfile.close()
 sfile = open(sfile.name, "r")
 tfile = open(tfile.name, "w")
@@ -116,7 +73,7 @@ proc = subprocess.Popen("hsi < %s" % tfile.name, shell=True)
 os.remove(tfile.name)
 
 # Check that the files retrieved, and untar
-for file in obs_files + var_files:
+for file in var_files:
     fpn = "%s/%s" % (working_directory, file)
     if not os.path.isfile(fpn):
         raise Exception("Missing file %s" % fpn)
