b88a0ffc226783b2a8d6141562e5b5c6a0c25fed
Philip Brohan <philip@brohan.org>
2021-03-11 16:14:10 +0000

    Added V3 v V4.6.3 comparison plot.

---
+++ analyses/Ed_RTPS/V3vV463_Ulysses_storm_1903/video/US_V3v463.py
@@ -0,0 +1,158 @@
+#!/usr/bin/env python
+
+# UK region weather plot
+# Compare pressures from 20CRV3 and 4.6.3
+# Video version.
+
+import os
+import math
+import datetime
+import numpy
+import pandas
+
+import iris
+import iris.analysis
+
+import matplotlib
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+
+import cartopy
+import cartopy.crs as ccrs
+
+import Meteorographica as mg
+import IRData.twcr as twcr
+
+# Get the datetime to plot from commandline arguments
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--year", help="Year", type=int, required=True)
+parser.add_argument("--month", help="Integer month", type=int, required=True)
+parser.add_argument("--day", help="Day of month", type=int, required=True)
+parser.add_argument(
+    "--hour", help="Time of day (0 to 23.99)", type=float, required=True
+)
+parser.add_argument(
+    "--opdir",
+    help="Directory for output files",
+    default="%s/images/V3vV463_Ulysses_storm" % os.getenv("SCRATCH"),
+    type=str,
+    required=False,
+)
+args = parser.parse_args()
+if not os.path.isdir(args.opdir):
+    os.makedirs(args.opdir)
+
+dte = datetime.datetime(
+    args.year, args.month, args.day, int(args.hour), int(args.hour % 1 * 60)
+)
+
+# HD video size 1920x1080
+aspect = 16.0 / 9.0
+fig = Figure(
+    figsize=(10.8 * aspect, 10.8),  # Width, Height (inches)
+    dpi=100,
+    facecolor=(0.88, 0.88, 0.88, 1),
+    edgecolor=None,
+    linewidth=0.0,
+    frameon=False,
+    subplotpars=None,
+    tight_layout=None,
+)
+canvas = FigureCanvas(fig)
+axb = fig.add_axes([0, 0, 1, 1])
+
+projection = ccrs.RotatedPole(pole_longitude=180, pole_latitude=35)
+scale = 15
+extent = [scale * -1 * aspect / 2, scale * aspect / 2, scale * -1, scale]
+
+# Two side-by-side plots
+ax_2c = fig.add_axes([0.01, 0.01, 0.485, 0.98], projection=projection)
+ax_2c.set_axis_off()
+ax_2c.set_extent(extent, crs=projection)
+ax_3 = fig.add_axes([0.505, 0.01, 0.485, 0.98], projection=projection)
+ax_3.set_axis_off()
+ax_3.set_extent(extent, crs=projection)
+
+# Background, grid and land for both
+ax_2c.patch.set_facecolor((0.88, 0.88, 0.88, 1))
+ax_3.patch.set_facecolor((0.88, 0.88, 0.88, 1))
+mg.background.add_grid(ax_2c)
+mg.background.add_grid(ax_3)
+land_img_2c = ax_2c.background_img(name="GreyT", resolution="low")
+land_img_3 = ax_3.background_img(name="GreyT", resolution="low")
+
+# Add the observations from v3
+obs = twcr.load_observations_fortime(dte, version="3")
+mg.observations.plot(ax_2c, obs, radius=0.1)
+
+# load the v3 pressures
+prmsl = twcr.load("PRMSL", dte, version="3")
+
+# Contour spread plot of ensemble members
+mg.pressure.plot(
+    ax_2c,
+    prmsl,
+    scale=0.01,
+    type="spread",
+    resolution=0.05,
+    levels=numpy.arange(870, 1050, 10),
+)
+
+# 20CRv3 label
+mg.utils.plot_label(
+    ax_2c,
+    "20CR v3",
+    facecolor=fig.get_facecolor(),
+    x_fraction=0.02,
+    horizontalalignment="left",
+)
+
+# 4.6.1 panel
+
+# Add the observations from 4.6.3
+obs = twcr.load_observations_fortime(dte, version="4.6.3")
+mg.observations.plot(ax_3, obs, radius=0.1)
+
+# load the 4.6.3 pressures
+prmsl = twcr.load("PRMSL", dte, version="4.6.3")
+
+# Contour spread plot of ensemble members
+mg.pressure.plot(
+    ax_3,
+    prmsl,
+    scale=0.01,
+    type="spread",
+    resolution=0.05,
+    levels=numpy.arange(870, 1050, 10),
+)
+
+mg.utils.plot_label(
+    ax_3,
+    "20CR 4.6.3",
+    facecolor=fig.get_facecolor(),
+    x_fraction=0.02,
+    horizontalalignment="left",
+)
+
+mg.utils.plot_label(
+    ax_3,
+    ("%04d-%02d-%02d:%02d" % (args.year, args.month, args.day, args.hour)),
+    facecolor=fig.get_facecolor(),
+    x_fraction=0.98,
+    horizontalalignment="right",
+)
+
+# Output as png
+fig.savefig(
+    "%s/V3vV463_Ulysses_storm_%04d%02d%02d%02d%02d.png"
+    % (
+        args.opdir,
+        args.year,
+        args.month,
+        args.day,
+        int(args.hour),
+        int(args.hour % 1 * 60),
+    )
+)
