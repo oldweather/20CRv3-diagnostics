52033129f1f619fb164e9b80f8cfc0845d7ad80a
Philip Brohan <philip@brohan.org>
2021-08-18 14:18:39 +0100

    Added script to retrieve annual v3 data from MASS.

---
+++ tools/extract_data/store_on_mass/v3_release_from_mass.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python
+
+# Retrieve archived 20CRv3 data from MASS
+
+import sys
+import os
+import subprocess
+import os.path
+import glob
+import re
+
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--year", help="Year", type=int, required=True)
+parser.add_argument(
+    "--variable",
+    help="Variable name ('prmsl','observations,...)",
+    type=str,
+    required=False,
+    default="all",
+)
+args = parser.parse_args()
+
+# Base location for storage
+mbase = "moose:/adhoc/projects/20cr/"
+moose_dir = "%s/version_3/%04d" % (mbase, args.year)
+
+# Disc data dir
+ddir = "%s/20CR/version_3/" % (os.environ["SCRATCH"])
+if not os.path.isdir(ddir):
+    os.makedirs(ddir)
+
+# Retrieve the observations
+def retrieve_obs(year):
+    # Are they on disc
+    ofiles = glob.glob("%s/observations/%04d/%04d*.txt" % (ddir, year, year))
+    if len(ofiles) > 100:
+        return  # Already on disc
+
+    proc = subprocess.Popen(
+        "moo ls %s/observations_%04d.tgz" % (moose_dir, year),
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        shell=True,
+    )
+    (out, err) = proc.communicate()
+    if len(err) != 0:
+        if variable == "observations":
+            raise Exception(
+                "Obs file not on mass %s/observations_%04d.tgz" % (moose_dir, year)
+            )
+        return
+
+    # Retrieve ob file from MASS
+    ody = "%s/observations/%04d" % (ddir, year)
+    if not os.path.isdir(ody):
+        os.makedirs(ody)
+    proc = subprocess.Popen(
+        "moo get %s/observations_%04d.tgz %s/observations_%04d.tgz"
+        % (moose_dir, year, ody, year),
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        shell=True,
+    )
+    (out, err) = proc.communicate()
+    if len(err) != 0:
+        print(err)
+        raise Exception("Failed to retrieve observations from %s" % moose_dir)
+    otarf = "%s/observations/%04d/observations_%04d.tgz" % (ddir, year, year)
+    proc = subprocess.Popen(
+        "cd %s ; tar mxzf %s" % (ddir, otarf),
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        shell=True,
+    )
+    (out, err) = proc.communicate()
+    if len(err) != 0:
+        print(err)
+        raise Exception("Failed to untar %s" % otarf)
+
+    # Clean up
+    os.remove(otarf)
+
+
+# Retrieve a variable file
+def retrieve_variable(year, variable):
+    vf = "%s/%04d/%s.%04d_mem080.nc" % (ddir, year, variable, year)
+    if os.path.isfile(vf):
+        return  # already on disc
+
+    if not os.path.isdir(os.path.dirname(vf)):
+        os.makedirs(os.path.dirname(vf))
+    proc = subprocess.Popen(
+        "moo get %s/%s_%04d.tar %s" % (moose_dir, variable, year, ddir),
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        shell=True,
+    )
+    (out, err) = proc.communicate()
+    if len(err) != 0:
+        print(err)
+        raise Exception(
+            "Failed to retrieve %s/%s_%04d.tar" % (moose_dir, variable, year)
+        )
+    otarf = "%s/%s_%04d.tar" % (ddir, variable, year)
+    proc = subprocess.Popen(
+        "cd %s ; tar mxf %s" % (ddir, otarf),
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        shell=True,
+    )
+    (out, err) = proc.communicate()
+    if len(err) != 0:
+        print(err)
+        raise Exception("Failed to untar %s" % otarf)
+
+    # Clean up
+    os.remove(otarf)
+
+
+# Retrieve a variable file
+def retrieve_variable(year, variable):
+    vf = "%s/%04d/%s.%04d_mem080.nc" % (ddir, year, variable, year)
+
+
+if args.variable == "observations":
+    retrieve_obs(args.year)
+else:
+    retrieve_variable(args.year, args.variable)
