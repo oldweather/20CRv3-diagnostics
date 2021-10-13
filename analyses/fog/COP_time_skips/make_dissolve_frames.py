425c4de05d422b8eb200e6751b8a91a512996224
Philip Brohan <philip@brohan.org>
2021-10-13 13:22:56 +0100

    Added script to make COP26-styled fog v. time video.

---
+++ analyses/fog/COP_time_skips/make_dissolve_frames.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+# Fill in the gaps in the frame list with dissolve transitions.
+
+import os
+import glob
+
+files = sorted(
+    glob.glob("%s/images/COP_time_skips/[0-9]*.png" % (os.getenv("SCRATCH")))
+)
+
+for idx in range(len(files) - 1):
+    f1 = os.path.basename(files[idx])
+    f2 = os.path.basename(files[idx + 1])
+    if f1[0:4] != f2[0:4]:
+        print(
+            ("./make_dissolve_transition.py --startfile=%s " + "--endfile=%s")
+            % (f1, f2)
+        )
